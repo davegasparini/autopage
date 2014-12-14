3a8c8c06600e88066d132f02f5272b5bf59ce8a0
davegasparini <davegasparini@gmail.com>
2014-12-14 11:27:51 -0500

    rm bodyparser

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var multer = require('multer');
 
 var routes = require('./routes/index');
@@ -18,8 +17,6 @@ app.set('view engine', 'ejs');
 
 app.use(favicon());
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
